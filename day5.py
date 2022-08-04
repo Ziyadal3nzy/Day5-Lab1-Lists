5c60e6646756032a80b81d9f930d435a6c62ac54
Ziyad <ziyadal3nzy@gmail.com>
2022-08-04 11:28:53 +0300

    Done

---
+++ day5.py
@@ -0,0 +1,7 @@
+my_list = [5, 4, 17, 19, 30, 2, 7, 10, 45]
+sum(my_list)
+max(my_list)
+even = [num for num in my_list if num % 2 == 0]
+print("Even numbers in the list: ", even)
+print(my_list[:5])
+
